cd5ad7a538bbd6747e55d9548e1034239df084a0
ApollineSetan <setan.apolline@gmail.com>
2025-03-14 10:59:38 +0100

    exercice throw exception corrige2

--- src/main/java/com/apolline/exercicespringboot/exception/SaveLivreExistException.java
+++ src/main/java/com/apolline/exercicespringboot/exception/SaveLivreExistException.java
@@ -2,6 +2,6 @@ package com.apolline.exercicespringboot.exception;
 
 public class SaveLivreExistException extends RuntimeException {
     public SaveLivreExistException() {
-        super("Le livre avec ces informations existe déjà en base de données.");
+        super("Un livre avec un titre similaire existe déjà en base de données.");
     }
 }
